7e0569f8a7b100396ebd8635f51f74892403ac38
JellyP <578291308@qq.com>
2018-06-16 20:30:30 +0800

    update 2018年6月16日20:30:19

---
+++ app/src/main/java/com/example/administrator/qfindhouse/PublishFindRoomActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.qfindhouse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PublishFindRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.publish_find_room);
+    }
+}
---
+++ app/src/main/java/com/example/administrator/qfindhouse/PublishRentRoomActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.qfindhouse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PublishRentRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.publish_rent_room);
+    }
+}
