cf4737e8697a3a2d3192716133ea362d8eb7bf27
RentRoom <447879326@qq.com>
2018-06-22 11:58:09 +0800

    update 2018/6/22 11:58

--- app/src/main/java/com/Service/DbService.java
+++ app/src/main/java/com/Service/DbService.java
@@ -1,4 +1,19 @@
 package com.Service;
 
-public class DbService {
+import com.Model.Room;
+
+import java.util.List;
+import java.util.Map;
+
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import rx.Observable;
+
+public interface DbService {
+    @GET("room/getRoomList")
+    Observable<List<Room>> getRoomList();
+
+//    @POST("user/login")
+//    Observable<Result> login(@Body Map<String,String> para);
 }
